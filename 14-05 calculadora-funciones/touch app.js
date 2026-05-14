93e595f0adf98f62a7d4bd18db95757da9acca83
Blandon104 <joseblandong@ielacandelariamedellin.edu.co>
2026-05-14 19:35:55 +0000

    TDM

---
+++ 14-05 calculadora-funciones/touch app.js
@@ -0,0 +1,41 @@
+// =====================================
+// CALCULADORA CON FUNCIONES
+// =====================================
+
+function sumar(a, b) {
+    return a + b;
+}
+
+function restar(a, b) {
+    return a - b;
+}
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function dividir(a, b) {
+    return a / b;
+}
+
+// =====================================
+// LLAMADO DE FUNCIONES
+// =====================================
+
+console.log("Resultado suma:");
+console.log(sumar(10, 5));
+
+console.log("----------------");
+
+console.log("Resultado resta:");
+console.log(restar(10, 5));
+
+console.log("----------------");
+
+console.log("Resultado multiplicación:");
+console.log(multiplicar(10, 5));
+
+console.log("----------------");
+
+console.log("Resultado división:");
+console.log(dividir(10, 5));
